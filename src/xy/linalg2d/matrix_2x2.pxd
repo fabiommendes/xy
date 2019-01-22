8decdf2ba45ff6809d62d9ab8ec6a0091e0e7e6a
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-01-22 15:22:08 -0200

    Update matrix module organization

--- src/xy/linalg2d/matrix_2x2.pxd
+++ src/xy/linalg2d/matrix_2x2.pxd
@@ -1,4 +1,5 @@
-from cpython cimport PyObject, PyTypeObject
+from cpython.object cimport PyObject, PyTypeObject, PyObject_TypeCheck
+cimport cython
 cimport vector_2d as V
 
 
@@ -39,6 +40,66 @@ cdef class Mat2:
 # UTILITY FUNCTIONS
 # ------------------------------------------------------------------------------
 
-cdef Mat2 fmat2(a, b, c, d)
-cdef Mat2 mat2(dmat2 v)
-cdef bint ismat(u)
+# Constructors -----------------------------------------------------------------
+cdef inline Mat2 flatmat(a, b, c, d):
+    cdef PyObject* new = _PyObject_New(Mat2Type)
+    (<Mat2> new).data = dmat2(a, c, b, d)
+    return <Mat2> new
+
+cdef inline Mat2 mat(dmat2 v):
+    cdef PyObject* new = _PyObject_New(Mat2Type)
+    (<Mat2> new).data = v
+    return <Mat2> new
+
+
+# Queries and assertions -------------------------------------------------------
+cdef inline bint ismat(u):
+    return PyObject_TypeCheck(u, Mat2Type)
+
+cdef inline bint istuple(u):
+    return PyObject_TypeCheck(u, TupleType)
+
+cdef inline bint isnull(dmat2 m):
+    return m == null.data
+
+cdef inline void with_shape(int n, int m, n_, m_):
+    if not ((n_ is None or n == n_) and (m_ is None or m == m_)):
+        raise ValueError(f'invalid shape: ({n_}, {m_})')
+
+
+# Data accessors ---------------------------------------------------------------
+cdef inline void set_mat(m, dmat2 *mat):
+    cdef tuple u, v
+    cdef double a, b, c, d
+
+    if ismat(m):
+        mat[0] = (<Mat2> m).data
+        return
+    elif istuple(m):
+        if len(<tuple> m) != 4:
+            with cython.boundscheck(False):
+                mat[0] = dmat2((<tuple> m)[0],
+                               (<tuple> m)[2],
+                               (<tuple> m)[1],
+                               (<tuple> m)[3])
+                return
+        elif len(<tuple> m) != 2:
+            with cython.boundscheck(False):
+                u = (<tuple> m)[0]
+                v = (<tuple> m)[1]
+            mat[0] = dmat2(u[0], v[0], u[1], u[1])
+    raise ValueError('Requires a Mat2, a 4-tuple or a tuple of 2-tuples.')
+
+cdef inline void set_abcd_(dmat2 m, double *a, double *b, double *c, double *d):
+    a[0] = m[0][1]
+    b[0] = m[1][0]
+    c[0] = m[0][1]
+    d[0] = m[1][1]
+
+
+# Constants --------------------------------------------------------------------
+cdef Mat2 identity = Mat2(1, 0, 0, 1)
+cdef Mat2 null = Mat2(0, 0, 0, 0)
+cdef PyTypeObject* Mat2Type = <PyTypeObject*> Mat2
+cdef PyTypeObject *TupleType = <PyTypeObject*> tuple
+cdef V.Direction2 ux = V.Direction2(1, 0)
--- src/xy/linalg2d/matrix_2x2.pyx
+++ src/xy/linalg2d/matrix_2x2.pyx
@@ -4,7 +4,6 @@
 include "../include/trigonometric.pxi"
 include "../include/matrix.pxi"
 cimport cython
-from cpython cimport PyObject_TypeCheck
 from libc.math cimport sqrt
 
 
@@ -29,7 +28,7 @@ cdef class Mat2:
 
     @property
     def T(self):
-        return mat2(transpose(self.data))
+        return mat(transpose(self.data))
 
     @property
     def x11(self):
@@ -54,7 +53,7 @@ cdef class Mat2:
     def from_flat(cls, data, nrows=None, ncols=None):
         with_shape(2, 2, nrows, ncols)
         a, b, c, d = data
-        return fmat2(a, b, c, d)
+        return flatmat(a, b, c, d)
 
     @classmethod
     def from_rows(cls, rows, nrows=None, ncols=None):
@@ -62,15 +61,15 @@ cdef class Mat2:
         row_a, row_b = rows
         a, b = row_a
         c, d = row_b
-        return fmat2(a, b, c, d)
+        return flatmat(a, b, c, d)
 
     @classmethod
     def from_dict(cls, dic, nrows=None, ncols=None):
         cdef dict dic_ = dict(dic)
         cdef Mat2 result
         with_shape(2, 2, nrows, ncols)
-        result = fmat2(dic_.pop((0, 0), 0), dic_.pop((0, 1), 0),
-                       dic_.pop((1, 0), 0), dic_.pop((1, 1), 0))
+        result = flatmat(dic_.pop((0, 0), 0), dic_.pop((0, 1), 0),
+                         dic_.pop((1, 0), 0), dic_.pop((1, 1), 0))
         if len(dic_) == 0:
             return result
         else:
@@ -83,42 +82,42 @@ cdef class Mat2:
         col_a, col_b = cols
         a, b = col_a
         c, d = col_b
-        return fmat2(a, c, b, d)
+        return flatmat(a, c, b, d)
 
     @classmethod
     def from_diag(cls, diag):
         a, b = diag
-        return fmat2(a, 0, 0, b)
+        return flatmat(a, 0, 0, b)
 
     @classmethod
     def rotation(cls, angle):
         cdef double cos, sin
         set_cs(angle, &cos, &sin)
-        return fmat2(cos, -sin, sin, cos)
+        return flatmat(cos, -sin, sin, cos)
 
     @classmethod
     def rotation_rad(cls, angle):
         cdef double cos = rcos(angle), sin = rsin(angle)
-        return fmat2(cos, -sin, sin, cos)
+        return flatmat(cos, -sin, sin, cos)
 
     @classmethod
     def scaling(cls, x, y=None):
         if y is None:
             y = x
-        return fmat2(x, 0, 0, y)
+        return flatmat(x, 0, 0, y)
 
     @classmethod
     def transformation(cls, angle=0, scale=1):
         cdef double cos, sin
         cdef double u = scale
         set_cs(angle, &cos, &sin)
-        return fmat2(u * cos, -u * sin, u * sin, u * cos)
+        return flatmat(u * cos, -u * sin, u * sin, u * cos)
 
     @classmethod
     def transformation_rad(cls, angle=0, scale=1):
         cdef double cos = rcos(angle), sin = rsin(angle)
         cdef double u = scale
-        return fmat2(u * cos, -u * sin, u * sin, u * cos)
+        return flatmat(u * cos, -u * sin, u * sin, u * cos)
 
     def __cinit__(self, a, b, c=None, d=None):
         if c is None:
@@ -129,15 +128,24 @@ cdef class Mat2:
     #
     # Magic methods
     #
-    def __len__(self): return 2
-    def __repr__(self): return mat_repr(self)
-    def __str__(self):  return mat_pretty(self)
-    def __abs__(self): return determinant(self.data)
-    def __neg__(self): return mat2(-self.data)
+    def __len__(self):
+        return 2
+
+    def __repr__(self):
+        return mat_repr(self)
+
+    def __str__(self):
+        return mat_pretty(self)
+
+    def __abs__(self):
+        return determinant(self.data)
+
+    def __neg__(self):
+        return mat(-self.data)
 
     def __iter__(self):
-        yield V.newvec2(self.data[0][0], self.data[1][0])
-        yield V.newvec2(self.data[0][1], self.data[1][1])
+        yield V.flatvec(self.data[0][0], self.data[1][0])
+        yield V.flatvec(self.data[0][1], self.data[1][1])
 
     def __eq__(u, v):
         if ismat(u) and ismat(v):
@@ -147,13 +155,13 @@ cdef class Mat2:
 
     def __add__(u, v):
         if ismat(u) and ismat(v):
-            return mat2((<Mat2> u).data + (<Mat2> v).data)
+            return mat((<Mat2> u).data + (<Mat2> v).data)
         else:
             return NotImplemented
 
     def __sub__(u, v):
         if ismat(u) and ismat(v):
-            return mat2((<Mat2> u).data - (<Mat2> v).data)
+            return mat((<Mat2> u).data - (<Mat2> v).data)
         else:
             return NotImplemented
 
@@ -161,22 +169,22 @@ cdef class Mat2:
         cdef double m
         if ismat(u):
             if ismat(v):
-                return mat2((<Mat2> u).data * (<Mat2> v).data)
+                return mat((<Mat2> u).data * (<Mat2> v).data)
             elif V.isvec(v):
-                return V.vec2((<Mat2> u).data * (<V.Vec2> v).data)
+                return V.vec((<Mat2> u).data * (<V.Vec2> v).data)
             else:
                 try:
                     m = v
-                    return mat2((<Mat2> u).data * m)
+                    return mat((<Mat2> u).data * m)
                 except TypeError:
                     return NotImplemented
         else:
             if V.isvec(u):
-                return V.vec2((<V.Vec2> u).data * (<Mat2> v).data)
+                return V.vec((<V.Vec2> u).data * (<Mat2> v).data)
             else:
                 try:
                     m = u
-                    return mat2((<Mat2> v).data * m)
+                    return mat((<Mat2> v).data * m)
                 except TypeError:
                     return NotImplemented
 
@@ -185,15 +193,15 @@ cdef class Mat2:
         if ismat(u):
             try:
                 m = v
-                return mat2((<Mat2> u).data / m)
+                return mat((<Mat2> u).data / m)
             except TypeError:
                 return NotImplemented
         return NotImplemented
 
 
     def cols(self):
-        yield V.vec2(self.data[0])
-        yield V.vec2(self.data[1])
+        yield V.vec(self.data[0])
+        yield V.vec(self.data[1])
 
     def items(self):
         yield (0, 0), self.x11
@@ -201,12 +209,23 @@ cdef class Mat2:
         yield (1, 0), self.x21
         yield (1, 1), self.x22
 
-    def det(self): return determinant(self.data)
-    def trace(self): return self.data[0][0] + self.data[1][1]
-    def diag(self): return V.newvec2(self.data[0][0], self.data[1][1])
-    def inv(self): return mat2(inverse(self.data))
-    def transpose(self): return mat2(transpose(self.data))
-    def solve(self, vec): return V.vec2(inverse(self.data) * V.tovec2(vec).data)
+    def det(self):
+        return determinant(self.data)
+
+    def trace(self):
+        return self.data[0][0] + self.data[1][1]
+
+    def diag(self):
+        return V.flatvec(self.data[0][0], self.data[1][1])
+
+    def inv(self):
+        return mat(inverse(self.data))
+
+    def transpose(self):
+        return mat(transpose(self.data))
+
+    def solve(self, vec):
+        return V.vec(inverse(self.data) * V.tovec(vec).data)
 
     def eigenvalues(self):
         cdef double a = self.data[0][0], \
@@ -239,32 +258,3 @@ cdef class Mat2:
             (l1, ux if denom1 == 0.0 else V.Direction2(b / denom1, 1)),
             (l2, ux if denom2 == 0.0 else V.Direction2(b / denom2, 1)),
         ]
-
-
-
-# ------------------------------------------------------------------------------
-# UTILITY FUNCTIONS
-# ------------------------------------------------------------------------------
-
-# Constructors
-cdef inline Mat2 fmat2(a, b, c, d):
-    cdef PyObject* new = _PyObject_New(Mat2Type)
-    (<Mat2> new).data = dmat2(a, c, b, d)
-    return <Mat2> new
-
-cdef inline Mat2 mat2(dmat2 v):
-    cdef PyObject* new = _PyObject_New(Mat2Type)
-    (<Mat2> new).data = v
-    return <Mat2> new
-
-# Queries and assertions
-cdef inline bint ismat(u): return PyObject_TypeCheck(u, Mat2Type)
-cdef inline void with_shape(int n, int m, n_, m_):
-    if not ((n_ is None or n == n_) and (m_ is None or m == m_)):
-        raise ValueError(f'invalid shape: ({n_}, {m_})')
-
-# Constants
-cdef Mat2 identity = Mat2(1, 0, 0, 1)
-cdef Mat2 null = Mat2(0, 0, 0, 0)
-cdef PyTypeObject* Mat2Type = <PyTypeObject*> Mat2
-cdef V.Direction2 ux = V.Direction2(1, 0)
